fc8055049c1ef0ba6c0ea3c35c6fdb075afe6bcc
sm.devguid <dev.guid@gmail.com>
2025-05-06 06:13:46 -0600

    Se agrega ajuste cors

--- backend/src/main/java/edu/sm_devguid/ecommerce/backend/infraestructure/rest/ProductController.java
+++ backend/src/main/java/edu/sm_devguid/ecommerce/backend/infraestructure/rest/ProductController.java
@@ -10,17 +10,42 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
+import java.math.BigDecimal;
+
 @RestController
 @RequestMapping("api/v1/admin/products")
 //http://localhost:8085/api/v1/admin/products
 @Slf4j
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200/")
 public class ProductController {
     private final ProductService productService;
 
     //http://localhost:8085/api/v1/admin/products
     @PostMapping
-    public ResponseEntity<Product> save(@RequestBody Product product){
+    public ResponseEntity<Product> save(
+                                        @RequestParam("id") Integer id,
+                                        @RequestParam("name") String name,
+                                        @RequestParam("code") String code,
+                                        @RequestParam("description") String description,
+                                        @RequestParam("price")BigDecimal price,
+                                        @RequestParam("urlImage") String urlImage,
+                                        @RequestParam("userId") Integer userId,
+                                        @RequestParam("categoryId")Integer categoryId){
+
+        Product product  = new Product();
+        if(id!= 0){
+            product.setId(id);
+        }
+        product.setName(name);
+        product.setCode(code);
+        product.setDescription(description);
+        product.setPrice(price);
+        product.setCategoryId(categoryId);
+        product.setUserId(userId);
+        product.setUrlImage(urlImage);
+
         log.info("Nombre producto: {}", product.getName());
         return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
     }
